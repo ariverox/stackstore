b89a770518df05d7144267ea13ff4a9ddc95b779
alex <alexisriverox@gmail.com>
2015-09-14 14:05:41 -0400

    did some stuff

--- server/api/users.js
+++ server/api/users.js
@@ -1,21 +1,24 @@
 var router =require('express').Router()
+var User = require('./db/models/user.model')
 
 
 //admin only - list of users
 router.get('/api/users', function(req,res){
-
-
-
-
+  User.find().then(function(users){
+    res.send(users)
+  })
 })
 
 //user page, specifc user, differnt for the user, other users and admin
 
 router.get('/api/users/:id', function(req,res){
+  var id = req.params.id;
+  User.findById(id).then(function(user){
+    res.send(user);
+  });
 
 
-
-})
+});
 
 
 
