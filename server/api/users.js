312997de276ea9ad794334587427f532a57b3eec
alex <alexisriverox@gmail.com>
2015-09-14 12:52:33 -0400

    changed some stuff

--- server/api/users.js
+++ server/api/users.js
@@ -1,1 +1,25 @@
 var router =require('express').Router()
+
+
+//admin only - list of users
+router.get('/api/users', function(req,res){
+
+
+
+
+})
+
+//user page, specifc user, differnt for the user, other users and admin
+
+router.get('/api/users/:id', function(req,res){
+
+
+
+})
+
+
+
+
+
+
+module.exports = router
