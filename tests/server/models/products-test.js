2eda3112b5a23289b2199b78c6ab3f556dbed031
= <danrlabrie@gmail.com>
2015-09-15 08:28:31 -0400

    finish product model tests

--- tests/server/models/products-test.js
+++ tests/server/models/products-test.js
@@ -22,126 +22,120 @@ describe('Products Model', function () {
     });
 
     it('should exist', function () {
-        expect(User).to.be.a('function');
+        expect(Products).to.be.a('function');
     });
 
-    describe('password encryption', function () {
+    describe('Products Schema', function () {
 
-        describe('generateSalt method', function () {
-
-            it('should exist', function () {
-                expect(User.generateSalt).to.be.a('function');
+        it('should require name', function (done) {
+            var product = new Product({
+                stock: 54
             });
-
-            it('should return a random string basically', function () {
-                expect(User.generateSalt()).to.be.a('string');
+            product.save(function (err, savedProduct) {
+                expect( err.message ).to.equal( 'Validation Failed' );
+                done();
             });
-
         });
-
-        describe('encryptPassword', function () {
-
-            var cryptoStub;
-            var hashUpdateSpy;
-            var hashDigestStub;
-            beforeEach(function () {
-
-                cryptoStub = sinon.stub(require('crypto'), 'createHash');
-
-                hashUpdateSpy = sinon.spy();
-                hashDigestStub = sinon.stub();
-
-                cryptoStub.returns({
-                    update: hashUpdateSpy,
-                    digest: hashDigestStub
-                });
-
+        it('should require qty (stock)', function (done) {
+            var product = new Product({
+                name: "Delicious whatsit"
             });
-
-            afterEach(function () {
-                cryptoStub.restore();
+            product.save(function (err, savedProduct) {
+                expect( err.message ).to.equal( 'Validation failed');
+                done();
             });
-
-            it('should exist', function () {
-                expect(User.encryptPassword).to.be.a('function');
-            });
-
-            it('should call crypto.createHash with "sha1"', function () {
-                User.encryptPassword('asldkjf', 'asd08uf2j');
-                expect(cryptoStub.calledWith('sha1')).to.be.ok;
-            });
-
-            it('should call hash.update with the first and second argument', function () {
-
-                var pass = 'testing';
-                var salt = '1093jf10j23ej===12j';
-
-                User.encryptPassword(pass, salt);
-
-                expect(hashUpdateSpy.getCall(0).args[0]).to.be.equal(pass);
-                expect(hashUpdateSpy.getCall(1).args[0]).to.be.equal(salt);
-
-            });
-
-            it('should call hash.digest with hex and return the result', function () {
-
-                var x = {};
-                hashDigestStub.returns(x);
-
-                var e = User.encryptPassword('sdlkfj', 'asldkjflksf');
-
-                expect(hashDigestStub.calledWith('hex')).to.be.ok;
-                expect(e).to.be.equal(x);
-
-            });
-
         });
-
-        describe('on creation', function () {
-
-            var encryptSpy;
-            var saltSpy;
-
-            var createUser = function () {
-                return User.create({ email: 'obama@gmail.com', password: 'potus' });
-            };
-
-            beforeEach(function () {
-                encryptSpy = sinon.spy(User, 'encryptPassword');
-                saltSpy = sinon.spy(User, 'generateSalt');
-            });
-
-            afterEach(function () {
-                encryptSpy.restore();
-                saltSpy.restore();
-            });
-
-            it('should call User.encryptPassword with the given password and generated salt', function (done) {
-                createUser().then(function () {
-                    var generatedSalt = saltSpy.getCall(0).returnValue;
-                    expect(encryptSpy.calledWith('potus', generatedSalt)).to.be.ok;
-                    done();
-                });
-            });
-
-            it('should set user.salt to the generated salt', function (done) {
-               createUser().then(function (user) {
-                   var generatedSalt = saltSpy.getCall(0).returnValue;
-                   expect(user.salt).to.be.equal(generatedSalt);
-                   done();
-               });
-            });
-
-            it('should set user.password to the encrypted password', function (done) {
-                createUser().then(function (user) {
-                    var createdPassword = encryptSpy.getCall(0).returnValue;
-                    expect(user.password).to.be.equal(createdPassword);
-                    done();
-                });
-            });
-
-        });
-
     });
 
+    describe('addProduct function', function () {
+
+        it('should add a product to the db', function () {
+            Product.addProduct({
+                name: "gummy panda",
+                stock: 12
+            });
+            Product.findOne({name: "gummy panda"}, function (err, foundProduct) {
+                expect( foundProduct.name ).to.equal('gummy panda');
+            });
+        });
+
+        it('should fail informatively if product exists', function () {
+            Product.addProduct({
+                name: "gummy panda",
+                stock: 12
+            });
+            Product.addProduct({
+                name: "gummy panda",
+                stock: 12
+            }, function (err, savedProduct) {
+                expect ( err.message ).to.equal('That product already exists!');
+            });
+        });
+    });
+
+     describe('removeProduct function', function () {
+        before( function () {
+            var testproduct = Product.addProduct({
+                name: "chocolate ostrich",
+                stock: 55
+            });
+        });
+
+        it('should correctly find a product by id', function () {
+            Product.removeProduct(testproduct._id, function (err, rmProduct) {
+                expect( rmProduct._id ).to.equal(testproduct._id); 
+            });
+        });
+        it('should fail informatively if product does not exist', function () {
+            Product.removeProduct('994b88387bbb332', function (err, rmProduct) {
+                expect( err.message ).to.equal('product does not exist.');
+            });
+        });
+
+        it('should successfully remove a product from the db', function () {
+            Product.findOne({name: "chocolate ostrich"}, function(err, product) {
+                expect(err.message).to.equal('product does not exist.');
+            });
+        });
+    });
+
+    describe('updateProduct function', function () {
+        beforeEach( function () {
+            var testproduct = Product.addProduct({
+                name: "crunchy bear ear",
+                stock: 32
+            });
+        });
+        it('should correctly find a product by id', function () {
+            Product.updateProduct(testproduct._id, {name: "sweet bear toe"}, function (err, upProduct) {
+                expect( upProduct._id ).to.equal(testproduct._id);
+            });
+        });
+
+        it('should correctly update that product in the db', function () {
+            Product.updateProduct(testproduct._id, {name: "sweet bear toe"}, function (err, upProduct) {
+                expect( upProduct.name ).to.equal("sweet bear toe");
+            });
+        });
+        it('should NOT create a new entry in the db', function () {
+            var dbSize;
+            Product.find({})
+            .then(function (all) {
+                dbSize = all.length;
+            })
+            .then(function() {
+                Product.updateProduct(testproduct._id, {name: "evil bear toe"});
+            })
+            .then(function() {
+                return Product.find({});
+            })
+            .then(function (allagain) {
+                    expect(allagain.length).to.equal(dbSize);
+        });
+        it('should fail informatively if product does not exist', function () {
+            Product.updateProduct("9b88444dc984a48b", {stock: 33}, function (err, upProduct) {
+                expect( err.message ).to.equal('product does not exist.');
+            });
+        });
+    });
 });
