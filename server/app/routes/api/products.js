2546855b4fa19ffff7545e76d8099d3e7f595a72
Alexis Rivero <ariverox@users.noreply.github.com>
2015-09-15 17:43:22 -0400

    Update products.js

--- server/app/routes/api/products.js
+++ server/app/routes/api/products.js
@@ -1,7 +1,27 @@
-var router =require('express').Router()
-
-router.get('/', function(req,res){
-  res.send('test')
-})
-
-module.exports= router
+-'use strict';
+-
+-var router = require('express').Router();
+-var Product = require('../db/models/product.model');
+-
+-var bodyParser = require('body-parser');
+-router.use(bodyParser.json());
+-router.use(bodyParser.urlencoded({extended: true}));
+-
+-
+-// get all products, or all products filtered by one or more optional queries
+-router.get('/api/products', function(req, res) {
+-	Product.find(req.query).then(function(products) {
+-		res.send(products);
+-	})
+-})
+-
+-//get a specific product by product ID
+-router.get('/api/products/:id', function(req, res) {
+-	Product.findById(req.params.id).then(function(product) {
+-		res.send(product);
+-	})
+-})
+-
+-
+-
+-module.exports = router; 
