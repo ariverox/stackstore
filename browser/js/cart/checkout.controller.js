8922dc2bf1ee82face26f85c552cfe5e87e3b6dc
jl975 <jlee6891@gmail.com>
2015-09-21 16:44:15 -0400

    stuff

--- browser/js/cart/checkout.controller.js
+++ browser/js/cart/checkout.controller.js
@@ -21,9 +21,9 @@ app.controller('CheckoutCtrl', function($scope, localStorageService, OrderFactor
         $scope.toCheckout.deliveryDate = new Date(Date.parse($scope.toCheckout.timestamp) + (24*60*60000)*4);     // 4 days after order is submitted
         $scope.toCheckout.subtotal = itemsInCart.reduce(((a, b) => a + (b.product.price * b.quantity)), 0);
 
-        $scope.toCheckout.name = $scope.user.name;
-        $scope.toCheckout.email = $scope.user.email;
-        $scope.toCheckout.shippingAddress = $scope.user.address;
+        $scope.toCheckout.name = $scope.user ? $scope.user.name : '';
+        $scope.toCheckout.email = $scope.user ? $scope.user.email : '';
+        $scope.toCheckout.shippingAddress = $scope.user ? $scope.user.address : '';
 
     }
 
@@ -33,24 +33,30 @@ app.controller('CheckoutCtrl', function($scope, localStorageService, OrderFactor
 
 
         var orderId;
+
+        // Update inventory stock for all purchased items
         Promise.all($scope.toCheckout.items.map(item => {
             return ProductFactory.update(item.product._id, {stock: item.product.stock - item.quantity});
         }))
         .then(function() {
+        // Submit the order
             $scope.toCheckout.items.forEach(item => {
                 item.product = item.product._id;
             })
             return OrderFactory.submitOrder($scope.toCheckout);          
         })
         .then(function(order) {
+        // Empty cart; if user is logged in, save order in user's order history
+            orderId = order._id;
             if ($scope.user) {
                 if (!$scope.user.orders) $scope.user.orders = [];
                 $scope.user.orders.push(order._id);
-                orderId = order._id;
                 return UserFactory.update($scope.user._id, {orders: $scope.user.orders, cart: []});
             }
+            else localStorageService.set('items', []);
         })
         .then(function() {
+        // Redirect to order confirmation page
             $state.go('confirmation', {id: orderId});
         })
 
